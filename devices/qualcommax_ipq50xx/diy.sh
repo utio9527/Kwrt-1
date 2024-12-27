66efda56d32554de7e4d519fabcb4076bcdefb93
utio9527 <74241727+utio9527@users.noreply.github.com>
2024-12-27 08:39:12 +0800

    Update diy.sh

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
-
 shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+
+rm -rf package/kernel/qca-* package/boot/uboot-envtools package/firmware/ipq-wifi package/firmware/ath11k-firmware
+
+git_clone_path ipq50xx-pr https://github.com/hzyitc/openwrt-redmi-ax3000 target/linux/ipq50xx package/firmware/ipq-wifi package/firmware/ath11k-firmware package/boot/uboot-envtools
+
+sed -i "s/wpad-basic-wolfssl/wpad-basic-mbedtls/" target/linux/ipq50xx/Makefile
